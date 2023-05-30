f49ef3b9cd8862d1d5b640b351b4553679a25425
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-05-30 08:58:33 +0800

    Create vmstart.bat

---
+++ vmstart.bat
@@ -0,0 +1,17 @@
+@echo off
+set PATH=%PATH%"C:\Program Files (x86)\VMware\VMware Player\";
+
+dir /ad
+set /p ans="Which vm do you want open?"
+
+if ans==""\ (
+  echo 'vmstart vm'
+  goto :eof
+)
+
+if exist %ans%\ (
+	vmrun start "C:\Users\bigred\vm\%ans%\%ans%.vmx"
+
+	echo "%ans% is poweron"
+)
+echo "vm - %ans% : is poweron" >> on.tmp
