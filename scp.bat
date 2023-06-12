3cec1e8f19127e2bb695921d7a1d7f245726bd3e
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-06-12 08:10:35 +0800

    Update scp.bat

--- scp.bat
+++ scp.bat
@@ -4,12 +4,12 @@
 :loop
 set /p ip="IP? "
 
+:: 案Q離開
+FOR %%a in (q Q) DO IF %ip%==%%a goto eof
+
 scp C:\Users\bigred\.ssh\id_ed25519.pub bigred@120.96.143.%ip%:%USERPROFILE%\.ssh\authorized_keys
 
-
 scp C:\Users\bigred\.ssh\id_ed25519.pub bigred@120.96.143.%ip%:C:\Users\rbean\.ssh\authorized_keys
-
-
 
 scp C:\Users\bigred\.ssh\id_ed25519.pub bigred@120.96.143.%ip%:C:\ProgramData\ssh\administrators_authorized_keys
 
