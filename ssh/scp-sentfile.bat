c623080f30e3707658a54605a6c0b5ff661d5a05
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-06-18 11:56:33 +0800

    Update scp-sentfile.bat

--- ssh/scp-sentfile.bat
+++ ssh/scp-sentfile.bat
@@ -1,11 +1,15 @@
 @echo off
 
+:: 輸入發送檔案名稱
 set /p name="please input scp file Name: "
 
+:: 讀取所有電腦 IP 提取後三碼
 for /F "tokens=4 delims=." %%c in (iplist.txt) do (
 	:: echo %%c
+:: scp 檔案到 rbean 家目錄
 	scp "%name%" bigred@120.96.143.%%c:C:\Users\rbean\
-	if ERRORLEVEL == 0 ( 
+:: 代碼執行成功 發送 ip + sent ok	
+if ERRORLEVEL == 0 ( 
 	echo %%c sent OK )
 pause
 )
